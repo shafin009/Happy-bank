ca876a5d238ef8fc2dfd61a50365a34b2091a0f7
shafin009 <ghostboyshafin@example.com>
2022-02-17 01:27:01 +0600

    commit

---
+++ script.js
@@ -0,0 +1,27 @@
+
+const myIncome = document.getElementById("income").value;
+const myIncomeValue = parseFloat(myIncome);
+console.log(myIncomeValue);
+
+const eatFood = document.getElementById("food").value;
+const eatFoodValue = parseFloat(eatFood);
+console.log(eatFoodValue);
+
+
+const myRent = document.getElementById("rent").value;
+const myRentValue = parseFloat(myRent);
+console.log(myRentValue);
+
+const myClothes = document.getElementById("clothes").value;
+const myClothesValue = parseFloat(myClothes);
+console.log(myClothesValue);
+
+
+document.getElementById("calculate").addEventListener('click', function () {
+
+
+
+
+
+
+})
