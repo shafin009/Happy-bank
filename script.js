f4c5211b02e724b88735f05e2a8c1f6de0a1cde3
shafin009 <ghostboyshafin@example.com>
2022-02-17 21:16:40 +0600

    finish

--- script.js
+++ script.js
@@ -2,49 +2,101 @@
 //----------------------Calculate Part---------------
 
 document.getElementById("calculate").addEventListener("click", function () {
-	const income = parseFloat(document.getElementById('income').value);
+	let income = parseFloat(document.getElementById('income').value);
+	let incomeSource = income;
 
+	//--------------error message-----------
+	if (isNaN(incomeSource) || incomeSource < 0) {
+		error('Please enter a valid number');
+	}
 
 	//------------------------Calculation--------------- 
-	const food = parseFloat(document.getElementById('food').value);
-	const rent = parseFloat(document.getElementById('rent').value);
-	const clothes = parseFloat(document.getElementById('clothes').value);
+	let food = parseFloat(document.getElementById('food').value);
+	let rent = parseFloat(document.getElementById('rent').value);
+	let clothes = parseFloat(document.getElementById('clothes').value);
 
-	const total = food + rent + clothes;
+	let foodValue = food;
+	let rentValue = rent;
+	let clothesValue = clothes;
+
+	if (isNaN(foodValue) || foodValue < 0) {
+		error('Please enter a valid number');
+	}
+	if (isNaN(rentValue) || rentValue < 0) {
+		error('Please enter a valid number');
+	}
+	if (isNaN(clothesValue) || clothesValue < 0) {
+		error('Please enter a valid number');
+	}
+
+
+	const total = foodValue + rentValue + clothesValue;
 	document.getElementById('expenses').innerText = total;
 
-	const Balance = income - total;
-	document.getElementById('balance').innerText = Balance;
+	const bill = incomeSource - total;
+	document.getElementById('balance').innerText = bill;
+
+	//--------------error message-----------
+
+	if (incomeSource < total) {
+		error('Error!!!')
+	}
 
 
 });
-
 
 // ------------------------ Save Part ---------------------
 
 document.getElementById('saving-part').addEventListener("click", function () {
 
 
-	const income = parseFloat(document.getElementById('income').value);
+	let income = parseFloat(document.getElementById('income').value);
+	let incomeSource = income;
 
+	let mybalance = parseFloat(document.getElementById('balance').value);
+	//------------------error message-------------
+	if (isNaN(incomeSource) || incomeSource < 0) {
+		error('Please enter a valid number!!!');
+	}
 
 	//------------------------Calculation--------------- 
-	const savingPercentage = parseFloat(document.getElementById('saving').value);
-	const savings = (income * savingPercentage) / 100;
+	let savingPercentage = parseFloat(document.getElementById('saving').value);
+	let savings = (incomeSource * savingPercentage) / 100;
+	let mySaving = savings;
 
-	document.getElementById('save').innerText = savings;
-	const food = parseFloat(document.getElementById('food').value);
-	const rent = parseFloat(document.getElementById('rent').value);
-	const clothes = parseFloat(document.getElementById('clothes').value);
-	const total = food + rent + clothes;
-	const balance = income - total;
-	const remainingBalance = balance - savings;
 
-	document.getElementById('remaining').innerText = remainingBalance;
+	//------------------error message--------------
+
+	if (isNaN(mySaving) || mySaving < 0) {
+		error('Please enter a valid number!!!');
+	}
+	//-----------------calculation-------------------
+	document.getElementById('save').innerText = mySaving;
+	const foodValue = parseFloat(document.getElementById('food').value);
+	const rentValue = parseFloat(document.getElementById('rent').value);
+	const clothesValue = parseFloat(document.getElementById('clothes').value);
+
+
+	//--------------error message---------------
+
+
+	const total = foodValue + rentValue + clothesValue;
+	const balance = incomeSource - total;
+	const remainingBalance = balance - mySaving;
+
+	if (remainingBalance < 0) {
+		error('Invalid!');
+	} else {
+
+		document.getElementById('remaining').innerText = remainingBalance;
+	}
+
+
 });
 
-
-
+function error(msg) {
+	return alert(msg);
+}
 
 
 
